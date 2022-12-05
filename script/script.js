bde0554ec9bedb8713b729d97f4edbc3fb807bf3
Aashray Katiyar <aashray446@gmail.com>
2022-12-05 22:08:44 +0530

    <INTRO : [BASIC CONTROL FLOW]

---
+++ script/script.js
@@ -0,0 +1,55 @@
+//  -------------------- VARIABLES --------------------
+let myVariable = 0;
+const myConstant = 1;
+
+// -------------------- LOGS  -------------------------------
+console.log(myVariable);
+console.log(myConstant);
+
+// -------------------- CHANGING VARIABLES
+myVariable = 2;
+console.log(myVariable);
+
+myConstant = 3; // ERROR: Assignment to constant variable.
+console.log(myConstant);
+
+// -------------------- FUNCTIONS------------------------------------------------
+
+// -------------------- FUNCTION DECLARATION ------------------------------------
+function myFunction() {
+    console.log('Hello World');
+}
+
+// -------------------- FUNCTION CALL ------------------------------------
+myFunction();
+myFunction();
+
+// -------------------- FUNCTION WITH PARAMETERS ------------------------------------
+function add(a, b) {
+    console.log(a + b);
+}
+add(1, 2);
+
+// -------------------- IF STATEMENT ------------------------------------
+
+if (1 == 1) {
+    console.log('Hello World');
+}
+else {
+    console.log('Goodbye World');
+}
+
+// -------------------- FOR LOOP ------------------------------------
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+// -------------------- WHILE LOOP ------------------------------------
+
+let i = 0;
+while (i < 10) {
+    console.log(i);
+    i++;
+}
+
